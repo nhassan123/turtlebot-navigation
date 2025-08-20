0041e4a9879d514a721af6ec1dfc0363f2327d30
najahh <najahhassan24@gmail.com>
2025-08-20 19:40:44 +0500

    feat:add readings from scanner

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using std::placeholders::_1;
 
@@ -9,6 +10,12 @@ public:
     TurtlebotController() : Node("turtlebot3_controller")
     {
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+        // Subscribe to laser scan topic
+        subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "scan", 10, std::bind(&TurtlebotController::scan_callback, this, _1));
+
+
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100),
             std::bind(&TurtlebotController::timer_callback, this)
@@ -19,15 +26,44 @@ public:
     }
 
 private:
+    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        int num_ranges = msg->ranges.size();
+        int mid_index = num_ranges / 2;
+
+        // Check 45 degree range around the front
+        double angle_range = 45.0 * M_PI / 180.0; // 15 degrees in radians
+        int samples_per_angle = static_cast<int>(angle_range / msg->angle_increment);
+
+        int start_index = std::max(0, mid_index - samples_per_angle);
+        int end_index = std::min(mid_index + samples_per_angle, num_ranges - 1);
+
+        obstacle_detected = false;
+        for (int i = start_index; i <= end_index; i++) {
+            if (msg->ranges[i] <= 0.2) {
+                obstacle_detected = true;
+                break;
+            }
+        }
+    }
+
     void timer_callback()
     {
         auto message = geometry_msgs::msg::Twist();
-        message.linear.x = 0.1;
+        if (obstacle_detected) {
+            message.linear.x = 0.0;
+            RCLCPP_WARN(this->get_logger(), "Obstacle detected ahead!");
+        }
+        else {
+            message.linear.x = 0.1;
+        }
 
         publisher_->publish(message);
     }
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    bool obstacle_detected;
 };
 
 
