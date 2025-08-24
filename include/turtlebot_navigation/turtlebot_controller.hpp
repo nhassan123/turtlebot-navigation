e4760f705f2157caa72eabd46d43070f19b40d7f
najahh <najahhassan24@gmail.com>
2025-08-24 15:15:27 +0500

    chore: code cleanup

--- include/turtlebot_navigation/turtlebot_controller.hpp
+++ include/turtlebot_navigation/turtlebot_controller.hpp
@@ -5,17 +5,17 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 
-class TurtlebotController: public rclcpp::Node
+class TurtlebotController : public rclcpp::Node
 {
-    public:
+public:
     TurtlebotController();
 
-    private:
+private:
     void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
     void timer_callback();
 
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _publisher;
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr _subscription;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _vel_pub;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr _laser_scan_sub;
     rclcpp::TimerBase::SharedPtr _timer;
 
     bool _obstacle_detected;
--- src/turtlebot_controller.cpp
+++ src/turtlebot_controller.cpp
@@ -6,12 +6,9 @@ const double DISTANCE_THRESHOLD = 0.5;
 
 TurtlebotController::TurtlebotController() : Node("turtlebot3_controller"), _obstacle_detected(false)
 {
-    _publisher = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-
-    // Subscribe to laser scan topic
-    _subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    _vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    _laser_scan_sub = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10, std::bind(&TurtlebotController::scan_callback, this, _1));
-
 
     _timer = this->create_wall_timer(
         std::chrono::milliseconds(100),
@@ -19,24 +16,25 @@ TurtlebotController::TurtlebotController() : Node("turtlebot3_controller"), _obs
     );
 
     RCLCPP_INFO(this->get_logger(), "Turtlebot3 Contoller Node has been started.");
-
 }
 
-
 void TurtlebotController::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    int num_ranges = msg->ranges.size();
+    int scanner_range_size = msg->ranges.size();
 
-    double angle_range_radians = ANGLE_HEADING_RANGE * M_PI / 180.0;
-    int samples_per_angle = static_cast<int>(angle_range_radians / msg->angle_increment);
+    double heading_range_radians = ANGLE_HEADING_RANGE * M_PI / 180.0;
+    int scan_section_size = static_cast<int>(heading_range_radians / msg->angle_increment);
 
     _obstacle_detected = false;
-    for (int i = 0; i <= samples_per_angle; i++) {
+    // scan on the right set of heading direction
+    for (int i = 0; i <= scan_section_size; i++) {
         if (msg->ranges[i] <= DISTANCE_THRESHOLD) {
             _obstacle_detected = true;
             return;
         }
     }
-    for (int i = num_ranges - samples_per_angle; i < num_ranges; i++) {
+    
+    // scan on the left side of heading direction
+    for (int i = scanner_range_size - scan_section_size; i < scanner_range_size; i++) {
         if (msg->ranges[i] <= DISTANCE_THRESHOLD) {
             _obstacle_detected = true;
             return;
@@ -55,5 +53,5 @@ void TurtlebotController::timer_callback()
         message.linear.x = 0.1;
     }
 
-    _publisher->publish(message);
+    _vel_pub->publish(message);
 }
